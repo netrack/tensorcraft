dd123eee19c1b95ea819a52663818bc241b26931
Yasha Bubnov <girokompass@gmail.com>
2019-06-11 15:47:59 +0300

    Update package requirements (#22)
    This patch updates package requirements.

--- bothe/logging.py
+++ bothe/logging.py
@@ -1,6 +1,7 @@
-import absl.logging
+import importlib
 import logging
 import os
+
 
 # Disable logging from TensorFlow CPP files.
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
@@ -9,11 +10,21 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 logging.getLogger("asyncio").disabled = True
 logging.getLogger("tensorflow").disabled = True
 
-# ABSL logging removes all root handlers and puts itself to the list of
-# root handlers, this dependency comes with TensorFlow, and we want to
-# modify this behaviour to prettify logs.
-for h in logging.root.handlers:
-    logging.root.removeHandler(h)
+
+try:
+    # This import is done in a hope that TensorFlow will drop this
+    # dependency in the future versions.
+    absl_logging = importlib.import_module("absl.logging")
+
+    # ABSL logging removes all root handlers and puts itself to the list of
+    # root handlers, this dependency comes with TensorFlow, and we want to
+    # modify this behaviour to make logs pretty and consistent.
+    for h in logging.root.handlers:
+        if isinstance(h, absl_logging.ABSLHandler):
+            logging.root.removeHandler(h)
+except ModuleNotFoundError:
+    pass
+    # Nothing to do.
 
 
 internal_format = "{asctime} {levelname} - {message}"
