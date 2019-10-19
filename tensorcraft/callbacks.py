87a4244cc6fcbe4a673eb51db50f1501d70cb73b
Yasha Bubnov <ybubnov@gojuno.com>
2019-10-19 11:54:31 +0300

    Add unit tests.

--- tensorcraft/callbacks.py
+++ tensorcraft/callbacks.py
@@ -1,16 +1,24 @@
 import pathlib
+import semver
+import tarfile
 import tempfile
 
 from tensorflow import keras
 from tensorflow.keras import callbacks
 
+from tensorcraft import asynclib
+from tensorcraft import client
+
 
 class ModelCheckpoint(callbacks.Callback):
-    """Publish model to server afer every epoch.
+    """Publish model to server after every epoch.
 
     Args:
-        name -- name of the model
-        tag -- tag of the model
+        name -- name of the model, when name is not given, name attribute of the
+                model will be used
+        tag -- tag of the model, by default is "0.0.0", every iteration will
+               bump build version, so on the next epoch version will be
+               "0.0.0+build1"; tag must be valid semantic version
         tls -- true to use TLS
         tlsverify -- use TLS and verify remote
         tlscacert -- trust certs signed only by this CA
@@ -19,11 +27,12 @@ class ModelCheckpoint(callbacks.Callback):
     """
 
     def __init__(self,
-                 name: str,
+                 name: str = None,
                  tag: str = "0.0.0",
+                 verbose: int = 0,
                  service_url: str = "http://localhost:5678",
                  tls: bool = False,
-                 tlsverify: bool = False
+                 tlsverify: bool = False,
                  tlscacert: pathlib.Path = "cacert.pem",
                  tlscert: pathlib.Path = "cert.pem",
                  tlskey: pathlib.Path = "key.pem"):
@@ -31,19 +40,34 @@ class ModelCheckpoint(callbacks.Callback):
 
         self.name = name
         self.tag = tag
-        self.client = Client.new(service_url=service_url,
-                                 tls=tls,
-                                 tlsverify=tlsverify,
-                                 tlscacert=tlscacert,
-                                 tlscert=tlscert,
-                                 tlskey=tlskey)
+        self.verbose = verbose
+        self.client = client.Client.new(service_url=service_url,
+                                        tls=tls,
+                                        tlsverify=tlsverify,
+                                        tlscacert=tlscacert,
+                                        tlscert=tlscert,
+                                        tlskey=tlskey)
 
     def on_epoch_end(self, epoch, logs=None) -> None:
         with tempfile.TemporaryDirectory() as td:
-            keras.experimental.export_save_model(self.model, td.name)
+            modelpath = pathlib.Path(td, "model")
+            keras.experimental.export_saved_model(self.model, str(modelpath))
 
-            self.tag = semver.bump_build(self.tag)
-            asyncreader = asynclib.reader(td.name)
+            tarpath = modelpath.with_suffix(".tar")
+            with tarfile.open(str(tarpath), mode="w") as tar:
+                tar.add(str(modelpath), arcname="")
 
-            coro = self.client.push(self.name, self.tag, asyncreader)
+            asyncreader = asynclib.reader(tarpath)
+
+            name = self.name or self.model.name
+            tag = semver.bump_build(self.tag)
+
+            if self.verbose > 0:
+                print("\nEpoch {0:5d}: pushing model {1}:{2}".
+                      format(epoch + 1, name, tag))
+
+            coro = self.client.push(name, tag, asyncreader)
             asynclib.run(coro)
+
+        # Update tag after successfull model publish.
+        self.tag = tag
