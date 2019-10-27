e97c47036964be06c12906028ca91594350fb9a2
Yasha Bubnov <ybubnov@gojuno.com>
2019-10-27 14:59:04 +0300

    Add initial documentation.

--- tensorcraft/arglib.py
+++
@@ -1,6 +0,0 @@
-import inspect
-
-
-def filter_callable_arguments(callable, **kwargs):
-    argnames = inspect.getfullargspec(callable)
-    return {k: v for k, v in kwargs.items() if k in argnames.args}
--- tests/clienttest.py
+++
@@ -1,9 +0,0 @@
-import unittest.mock
-
-from tensorcraft import client
-from tests import asynctest
-
-
-def unittest_mock_client(method: str):
-    return unittest.mock.patch.object(client.Client, method,
-                                      new_callable=asynctest.AsyncMagicMock)
